101d905e01f540d9e4e115c594632b16c9d8ed74
Muhammad Salif <muhammad.salif@yahoo.com>
2020-07-27 17:29:59 +0500

    UI Changing & new functionality added of bootstrap.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { getQuizDetails } from "./services/Quiz_service";
 import { QuestionType } from "./types/Quiz_types";
 import { QuestionCard } from "./components/QuestionCard";
 import HashLoader from "react-spinners/HashLoader";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 function App() {
   let [quiz, setQuiz] = useState<QuestionType[]>([]);
@@ -13,7 +14,7 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       const questions: QuestionType[] = await getQuizDetails(3, "hard");
-      console.log("Your questions:", questions);
+      // console.log("Your questions:", questions);
       setQuiz(questions);
     }
     fetchData();
@@ -23,21 +24,21 @@ function App() {
     e.preventDefault();
 
     const currentQuestion = quiz[currentStep];
-    console.log(
-      "Correct ans is :" +
-        currentQuestion.correct_answer +
-        "User selection" +
-        userAns
-    );
+    // console.log(
+    //   "Correct ans is: " +
+    //     currentQuestion.correct_answer +
+    //     "User selection is: " +
+    //     userAns
+    // );
     if (userAns === currentQuestion.correct_answer) {
       setScore(++score);
     }
     if (currentStep !== quiz.length - 1) setCurrentStep(++currentStep);
     else {
       alert(
-        "Quiz is completed your final score is :" +
+        "Quiz is completed your final score is : " +
           score +
-          "---out of :" +
+          " Out of : " +
           quiz.length
       );
       setCurrentStep(0);
@@ -47,17 +48,29 @@ function App() {
 
   if (!quiz.length)
     return (
-      <div>
-        <HashLoader color={"#123abc"}></HashLoader>
+      <div className="container" style={{ height: "100vh" }}>
+        <div className="row h-100">
+          <div className="col-10 col-sm-8 col-md-6 col-lg-4 mx-auto d-flex align-items-center justify-content-center">
+            <HashLoader color={"#123abc"} size="80px"></HashLoader>
+          </div>
+        </div>
       </div>
     );
   return (
-    <div>
-      <QuestionCard
-        options={quiz[currentStep].option}
-        question={quiz[currentStep].question}
-        callback={handleSubmit}
-      ></QuestionCard>
+    <div className="container" style={{ height: "100vh" }}>
+      <div className="row d-flex align-items-center justify-content-center">
+        <div className="col-12 col-sm-11 col-md-9 col-lg-8 d-flex justify-content-center align-items-end my-4">
+          <span>
+            <h1 className="text-white">Quiz Application</h1>
+          </span>
+        </div>
+
+        <QuestionCard
+          options={quiz[currentStep].option}
+          question={quiz[currentStep].question}
+          callback={handleSubmit}
+        ></QuestionCard>
+      </div>
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -6,8 +6,6 @@ export const QuestionCard: React.FC<questionPropsType> = ({
   options,
   callback,
 }) => {
-  // console.log(question, options);
-
   let [selectedAns, setSelectedAns] = useState("");
 
   const handleSelection = (e: any) => {
@@ -15,10 +13,20 @@ export const QuestionCard: React.FC<questionPropsType> = ({
   };
 
   return (
-    <div>
-      <div className="question-container">
-        <div className="question">{question}</div>
+    <div className="col-12 col-sm-11 col-md-9 col-lg-8 d-flex justify-content-center align-items-center flex-column m-auto shadow py-5 questionCard rounded">
+      {/* Heading div */}
+
+      <div className="text-center bg-transparent">
+        <h4 className="mb-2 text-white bg-transparent font-weight-bold">
+          {question}
+        </h4>
+      </div>
+
+      {/* Form div */}
+      <div className="w-100 ">
         <form
+          style={{ width: "100%" }}
+          className="questionCard"
           onSubmit={(e: React.FormEvent<EventTarget>) =>
             callback(e, selectedAns)
           }
@@ -27,22 +35,35 @@ export const QuestionCard: React.FC<questionPropsType> = ({
         >
           {options.map((opt: string, ind: number) => {
             return (
-              <div key={ind}>
-                <label htmlFor="">
+              <div key={ind} className="d-flex questionCard">
+                <label
+                  className="col-12 col-sm-11 col-md-9 p-0 col-lg-8 my-2 btn  mx-auto btn-success "
+                  htmlFor=""
+                >
                   <input
-                    type="radio"
+                    // type="radio"
+                    type="button"
                     name="opt"
                     value={opt}
                     onChange={handleSelection}
-                    required
+                    // required
                     checked={selectedAns === opt}
-                  />
-                  {opt}
+                    onClick={handleSelection}
+                    className="bg-success border-0 text-white w-100 h-100 py-2"
+                  ></input>
                 </label>
               </div>
             );
           })}
-          <input type="submit" />
+          <div className="questionCard d-flex">
+            <button
+              className="col-12 col-sm-11 col-md-9 col-lg-8 my-2 text-white font-weight-bolder btn mx-auto "
+              style={{ backgroundColor: "#123abc" }}
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </div>
